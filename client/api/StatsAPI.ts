3d5d64d3ae354774e72fc4d29314326ea8e60021
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-03-30 20:19:35 -0700

    Add rest of the aggreagtion queries

--- client/api/StatsAPI.ts
+++ client/api/StatsAPI.ts
@@ -5,7 +5,34 @@ const server_port = import.meta.env.VITE_SERVER_URL || 'http://localhost:4000';
 
 export async function fetchMeasureOfSalesPerGroup(group: string, measure: string): Promise<AggregationGroup[]> {
     try {
-        const response = await axios.get(`${server_port}/stats/${group}/sales/${measure}`);
+        const response = await axios.get(`${server_port}/stats/${group.toLowerCase()}/sales/${measure}`);
+        return response.data as AggregationGroup[];
+    } catch (e) {
+        console.error(e.message);
+        return [];
+    }
+}
+
+export async function fetchCountByGroup(group: string, operation: string, count: number): Promise<AggregationGroup[]> {
+    try {
+        const response = await axios.post(
+            `${server_port}/stats/${group.toLowerCase()}/count`,
+            {
+                count: count,
+                operation: operation
+            }
+        )
+        return response.data as AggregationGroup[];
+    } catch (e) {
+        console.error(e.message);
+        return [];
+    }
+}
+
+export async function fetchExtremaByGroup(group: string, measure: string, extrema: string) {
+    try {
+        const response = await axios.get(
+            `${server_port}/stats/${group.toLowerCase()}/sales/${measure}/${extrema.toLowerCase()}`)
         return response.data as AggregationGroup[];
     } catch (e) {
         console.error(e.message);
--- client/pages/stats/index.vue
+++ client/pages/stats/index.vue
@@ -2,31 +2,82 @@
 
 import {ref} from "vue";
 import {AggregationGroup} from "~/types/types";
-import {fetchMeasureOfSalesPerGroup} from "~/api/StatsAPI";
+import {fetchCountByGroup, fetchExtremaByGroup, fetchMeasureOfSalesPerGroup} from "~/api/StatsAPI";
+
+enum Menu {
+  MEASURE, COUNT, EXTREMA
+}
+
+
+const currentMenu = ref<Menu>(Menu.MEASURE)
 
 const groups = ["Franchise", "Rating", "Genre"];
 const measurements = new Map<string, string>([["Total Sales", "sum"], ["Average Sales", "avg"]])
 const selectedGroup = ref<string>("Select Group");
 const selectedMeasure = ref<string>("Select Measure");
+const selectedOperator = ref<string>("Select Operator");
+const selectedExtrema = ref<string>("Select Extrema");
 const aggregations = ref<AggregationGroup[]>([]);
+const countInput = ref<string>("");
 
 const tableVisible = ref<boolean>(false);
 const warningVisible = ref<boolean>(false);
 
+const operators = new Map<string, string>([
+    ["GREATER THAN", ">"],
+    ["LESS THAN", "<"],
+    ["EQUAL TO", "="],
+    ["NOT EQUAL TO", "!="]
+])
+
+  function changeMenu(menu: Menu) {
+    currentMenu.value = menu;
+  }
+
   async function getSalesByGroup() {
-    if (validInput()) {
+    if (selectedGroup.value !== "Select Group" && selectedMeasure.value !== "Select Measure") {
       const measure: string = measurements.get(selectedMeasure.value);
-      aggregations.value = await fetchMeasureOfSalesPerGroup(selectedGroup.value.toLowerCase(), measure);
+      aggregations.value = await fetchMeasureOfSalesPerGroup(selectedGroup.value, measure);
       warningVisible.value = false;
+      tableVisible.value = true;
     } else {
       aggregations.value = [];
       warningVisible.value = true;
+      tableVisible.value = false;
     }
-    tableVisible.value = true;
   }
 
-  function validInput(): boolean {
-    return selectedGroup.value !== "Select Group" && selectedMeasure.value !== "Select Measure";
+  async function findExtremaByGroup() {
+    if (selectedGroup.value !== "Select Group" &&
+        selectedMeasure.value !== "Select Measure" && selectedExtrema.value !== "Select Extrema") {
+      aggregations.value = await fetchExtremaByGroup(
+          selectedGroup.value, measurements.get(selectedMeasure.value), selectedExtrema.value)
+      warningVisible.value = false;
+      tableVisible.value = true;
+    } else {
+      aggregations.value = [];
+      warningVisible.value = true;
+      tableVisible.value = false;
+    }
+  }
+
+  async function countVideoGamesByGroup() {
+    let validCount;
+    let count;
+    try {
+      count = parseInt(countInput.value)
+      validCount = !isNaN(count);
+    } catch (e) {
+      validCount = false;
+    }
+    if (validCount && selectedGroup.value !== "Select Group" && selectedOperator.value !== "Select Operator") {
+      aggregations.value = await fetchCountByGroup(selectedGroup.value, operators.get(selectedOperator.value), count)
+      warningVisible.value = false;
+      tableVisible.value = true;
+    } else {
+      warningVisible.value = true;
+      tableVisible.value = false;
+    }
   }
 
 
@@ -35,8 +86,16 @@ const warningVisible = ref<boolean>(false);
 <template>
 	<div class="statsContainer">
     <h1>Stats</h1>
-    <h3>Sales</h3>
-    <div class="aggregation-tabs">
+    <div class="menu">
+      <ul @click="warningVisible=false; tableVisible=false;">
+        <li @click="changeMenu(Menu.MEASURE)">Measure Sales</li>
+        <li @click="changeMenu(Menu.COUNT)">Count Groups</li>
+        <li @click="changeMenu(Menu.EXTREMA)">Maximum/Minimum Sales</li>
+      </ul>
+    </div>
+
+    <div class="aggregation-tab" v-if="currentMenu===Menu.MEASURE">
+      <h3>Measure Sales</h3>
       <select class="dropdown-menu" v-model="selectedGroup" @change="tableVisible = false">
         <option selected="selected">Select Group</option>
         <option v-for="group in groups">{{ group }}</option>
@@ -46,25 +105,83 @@ const warningVisible = ref<boolean>(false);
         <option v-for="measure in measurements.keys()">{{ measure }}</option>
       </select>
       <input type="submit" value="Query" @click="getSalesByGroup">
-      <table>
+      <table v-if="tableVisible">
         <tr>
           <th>{{ selectedGroup }}</th>
           <th>{{ selectedMeasure }} ($)</th>
         </tr>
-        <tbody v-if="tableVisible">
+        <tbody>
         <tr v-for="aggregation in aggregations" :key="aggregation.category">
           <td>{{ aggregation.category }}</td>
           <td>{{ aggregation.result }}</td>
         </tr>
         </tbody>
       </table>
-
-      <div class="alert" v-if="warningVisible">
-        <span class="closebtn" @click="warningVisible=false">&times;</span>
-        <strong>Invalid input selected (っ °Д °;)っ</strong>
-      </div>
-
     </div>
+
+    <div class="aggregation-tab" v-if="currentMenu===Menu.COUNT">
+      <h3>Count Number of Video Games Per Group</h3>
+      Video Games By
+      <select class="dropdown-menu" v-model="selectedGroup" @change="tableVisible = false">
+        <option selected="selected">Select Group</option>
+        <option v-for="group in groups">{{ group }}</option>
+      </select>
+      With Count
+      <select v-model="selectedOperator" @change="tableVisible=false">
+        <option selected="selected">Select Operator</option>
+        <option v-for="op in operators.keys()">{{ op }}</option>
+      </select>
+      <input type="text" v-model="countInput">
+      <input type="submit" value="Query" @click="countVideoGamesByGroup">
+      <table v-if="tableVisible">
+        <tr>
+          <th>{{ selectedGroup }}</th>
+          <th>Count</th>
+        </tr>
+        <tbody>
+        <tr v-for="aggregation in aggregations" :key="aggregation.category">
+          <td>{{ aggregation.category }}</td>
+          <td>{{ aggregation.result }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="aggregation-tab" v-if="currentMenu===Menu.EXTREMA">
+      <h3>Find Max/Min Sales</h3>
+      <select class="dropdown-menu" v-model="selectedGroup" @change="tableVisible=false">
+        <option selected="selected">Select Group</option>
+        <option v-for="group in groups">{{ group }}</option>
+      </select>
+      <select v-model="selectedMeasure" @change="tableVisible=false">
+        <option selected="selected">Select Measure</option>
+        <option v-for="measure in measurements.keys()">{{ measure }}</option>
+      </select>
+      <select v-model="selectedExtrema" @change="tableVisible=false">
+        <option selected="selected">Select Extrema</option>
+        <option>MAX</option>
+        <option>MIN</option>
+      </select>
+      <input type="submit" value="Query" @click="findExtremaByGroup">
+      <table  v-if="tableVisible">
+        <tr>
+          <th>{{ selectedGroup }}</th>
+          <th> {{ selectedExtrema }} {{ selectedMeasure }} ($)</th>
+        </tr>
+        <tbody>
+        <tr v-for="aggregation in aggregations" :key="aggregation.category">
+          <td>{{ aggregation.category }}</td>
+          <td>{{ aggregation.result }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="alert" v-if="warningVisible">
+      <span class="closebtn" @click="warningVisible=false">&times;</span>
+      <strong>Invalid input selected (っ °Д °;)っ</strong>
+    </div>
+
   </div>
 </template>
 
@@ -110,17 +227,15 @@ th, td {
 
 table {
   width: 50%;
-  /*margin-left: -10%;*/
   border-collapse: collapse;
 }
 
 .alert {
+  margin-top: 10px;
   padding: 15px;
-  /*margin-left: -10%;*/
   width: 50%;
   background-color: #f44336;
   color: white;
-  margin-bottom: 15px;
   align-self: flex-start;
 }
 
@@ -143,4 +258,16 @@ strong {
   font-weight: bold;
 }
 
+.menu {
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+
+ul {
+  list-style: none;
+}
+
+li {
+  margin-bottom: 5px;
+}
 </style>
